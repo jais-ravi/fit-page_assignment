ddd96bf2dc22d87ba94cee2c7e178e97a14ad947
Ravi Jaiswal <ravijaiswal258852@gmail.com>
2025-06-19 01:15:35 +0530

    fix

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -25,7 +25,7 @@ router.get(
     res.cookie("token",token,{
       httpOnly:true,
       secure : process.env.NODE_ENV === "production",
-      sameSite: "None",
+      // sameSite: "lax",
       maxAge : 7*24*60*60*1000,
     })
 
